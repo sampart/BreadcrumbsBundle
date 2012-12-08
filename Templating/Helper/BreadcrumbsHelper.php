7453999fbcaec028820799e2ac1e06bebcb6c2a2
Rich Sage <rich.sage@gmail.com>
2012-12-08 19:47:43 +0000

    Added options into helper.

--- Templating/Helper/BreadcrumbsHelper.php
+++ Templating/Helper/BreadcrumbsHelper.php
@@ -10,17 +10,24 @@ class BreadcrumbsHelper extends Helper
 {
     protected $templating;
     protected $breadcrumbs;
+    protected $options = array();
 
-    public function __construct(EngineInterface $templating, Breadcrumbs $breadcrumbs)
+    /**
+     * @param \Symfony\Component\Templating\EngineInterface $templating
+     * @param \WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs $breadcrumbs
+     * @param array $options
+     */
+    public function __construct(EngineInterface $templating, Breadcrumbs $breadcrumbs, array $options)
     {
         $this->templating  = $templating;
         $this->breadcrumbs = $breadcrumbs;
+        $this->options = $options;
     }
 
     /**
      * Returns the HTML for the breadcrumbs
      *
-     * @param $name
+     * @param $options
      * @return string A HTML string
      */
     public function breadcrumbs(array $options = array())
@@ -39,18 +46,16 @@ class BreadcrumbsHelper extends Helper
         return 'breadcrumbs';
     }
 
+    /**
+     * Merges user-supplied options from the view
+     * with base config values
+     *
+     * @param array $options
+     * @return array
+     */
     private function resolveOptions(array $options = array())
     {
-        return array_merge(
-                array(
-                        'breadcrumbs' => $this->breadcrumbs,
-                        'translation_domain' => null,
-                        'locale' => null,
-                        'listId' => 'wo-breadcrumbs',
-                        'listClass' => 'breadcrumb',
-                        'separator' => '/',
-                        'separatorClass' => 'separator',
-                ), $options
-        );
+        $this->options["breadcrumbs"] = $this->breadcrumbs;
+        return array_merge($this->options, $options);
     }
 }
