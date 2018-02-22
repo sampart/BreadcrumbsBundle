e2a883088afe3aad2600c6baa38f6cad90a59238
balazslevi <balazs.levi@gmail.com>
2018-02-22 10:18:06 +0200

    review changes

--- Test/BundleTest.php
+++ Test/BundleTest.php
@@ -1,8 +1,5 @@
 <?php
-if (!class_exists('\PHPUnit_Framework_TestCase') &&
-    class_exists('\PHPUnit\Framework\TestCase')) {
-    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
-}
+
 class BundleTest extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
 {
     public function testInitBundle()
@@ -11,7 +8,7 @@ class BundleTest extends \Symfony\Bundle\FrameworkBundle\Test\WebTestCase
 
         $container = $client->getContainer();
 
-        // Test if your services exists
+        // Test if the service exists
         $this->assertTrue($container->has('white_october_breadcrumbs.helper'));
 
         $service = $container->get('white_october_breadcrumbs.helper');
